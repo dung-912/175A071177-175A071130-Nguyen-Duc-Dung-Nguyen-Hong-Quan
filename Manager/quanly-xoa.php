c07d1529fbed896aac0374c8a6188e9cdab09e78
dung-912 <dungnd85@wru.vn>
2020-11-13 19:42:11 +0700

    chinh sua admin

--- Manager/quanly-xoa.php
+++
@@ -1,8 +0,0 @@
-<?php
-	require_once '../Chucnang/ketnoiSQL.php';
-	
-	$id = $_POST['id'];
-	
-	$conn->query("DELETE FROM `users` WHERE `usersID` = '$id'");
-	
-?>
--- Manager/tintuc-xoa.php
+++
@@ -1,6 +0,0 @@
-<?php
-	require_once '../Chucnang/ketnoiSQL.php';
-	
-	$id = $_POST['id'];
-	$conn->query("DELETE FROM `news` WHERE `newID` = '$id'");
-?>
